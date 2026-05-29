510cc20b17ecf1308442e6e38be9a0ecc933b9a9
Karthikeyan <whitekarthi02142@gmail.com>
2026-05-29 11:01:09 +0530

    Array

---
+++ differentconcate.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class differentconcate{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Entert the value: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        int[] arr1 = new int[size];
+        int[] res  = new int[size + size];
+        for (int i=0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        for (int i=0;i<size;i++){
+            arr1[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr1));
+ 
+        for (int i=0;i<size;i++){
+            res[i] = arr[i];
+        }
+
+        for (int i=0;i<size;i++){
+            res[i+size] = arr[i];
+        }
+        System.out.println(Arrays.toString(res));
+    }
+}
